9e1e0a8b8a470fe1983d00b5af9b91d485c2de9d
Cutean Bogdan Mihai <bogdancutean@proton.me>
2024-08-11 13:58:06 +0300

    Add - Timer system for roulette loop

--- src/sockets/rouletteManager.ts
+++ src/sockets/rouletteManager.ts
@@ -8,9 +8,9 @@ import {
 
 class RouletteManager {
   private isRolling = false;
-  private lastExecutionTime: number = Date.now();
   private io: Server;
   private socketUserMap: Map<string, string>;
+  private timer: number = 15;
 
   constructor(io: Server, socketUserMap: Map<string, string>) {
     this.io = io;
@@ -20,52 +20,52 @@ class RouletteManager {
 
   private async startRouletteLoop() {
     setInterval(async () => {
-      const currentTime = Date.now();
-      if (this.isRolling || currentTime - this.lastExecutionTime < 15000) {
-        logger.warn(
-          "Roulette process is already running or too soon to start a new one."
-        );
-        return;
-      }
+      if (this.isRolling) return;
 
-      this.isRolling = true;
-      this.lastExecutionTime = currentTime;
+      if (this.timer > 0) {
+        this.io.emit("timer-update", this.timer);
+        this.timer--;
+      } else {
+        this.isRolling = true;
+        this.timer = 15; // Reset the timer
 
-      try {
-        const result = await determineWinner();
+        try {
+          const result = await determineWinner();
+          const updatedHistory = await getUpdatedHistory();
 
-        const updatedHistory = await getUpdatedHistory();
+          this.io.emit("roulette-result", {
+            winningNumber: result.winningNumber,
+            winningColor: result.winningColor,
+            updatedHistory,
+          });
+          logger.info(`Roulette result: ${JSON.stringify(result)}`);
 
-        this.io.emit("roulette-result", {
-          winningNumber: result.winningNumber,
-          winningColor: result.winningColor,
-          updatedHistory,
-        });
-        logger.info(`Roulette result: ${JSON.stringify(result)}`);
-
-        // After determining the winner, emit the updated balances
-        for (const user of result.winningUsers) {
-          const socketId = [...this.socketUserMap.entries()].find(
-            ([key, value]) => value === user._id.toString()
-          )?.[0];
-          if (socketId) {
-            this.io.to(socketId).emit("balance-updated", user.balance);
-          } else {
-            logger.warn(`Socket ID not found for user ${user._id.toString()}`);
+          // After determining the winner, emit the updated balances
+          for (const user of result.winningUsers) {
+            const socketId = [...this.socketUserMap.entries()].find(
+              ([key, value]) => value === user._id.toString()
+            )?.[0];
+            if (socketId) {
+              this.io.to(socketId).emit("balance-updated", user.balance);
+            } else {
+              logger.warn(
+                `Socket ID not found for user ${user._id.toString()}`
+              );
+            }
           }
-        }
 
-        // Reset bets after the result
-        await resetBets();
-        this.io.emit("clear-bets");
-        logger.info("Bets reset after round completion");
-      } catch (error) {
-        this.io.emit("error", { message: "Error during roulette process" });
-        logger.error(`Error during roulette process: ${error}`);
-      } finally {
-        this.isRolling = false;
+          // Reset bets after the result
+          await resetBets();
+          this.io.emit("clear-bets");
+          logger.info("Bets reset after round completion");
+        } catch (error) {
+          this.io.emit("error", { message: "Error during roulette process" });
+          logger.error(`Error during roulette process: ${error}`);
+        } finally {
+          this.isRolling = false;
+        }
       }
-    }, 15000);
+    }, 1000);
   }
 }
 
